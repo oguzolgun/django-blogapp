6a90fed8f102255f80492e33fd3295e11dae1c2d
oguzolgun <oguzolgun44@gmail.com>
2022-06-06 23:29:49 +0300

    updated all files

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-class UserProfile(models.Model):
-    profile_pic = models.ImageField(upload_to="profile_pics",blank=True)
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    
-    def __str__(self) :
-        return self.user.username
+def user_profile_path(instance, filename):
+    return 'user/{0}/{1}'.format(instance.user.id, filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to= user_profile_path, default="avatar.png")
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{} {}".format(self.user, 'Profile')
--- users/views.py
+++ users/views.py
@@ -1,61 +1,35 @@
-from django.shortcuts import render, redirect, HttpResponse
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
-from django.contrib.auth import logout, login, authenticate
-from .forms import UserForm, UserProfileForm
-
-from django.contrib.auth.forms import AuthenticationForm
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'users/home.html')
-
-def user_logout(request):
-    messages.success(request, 'You logged out!')
-    logout(request)
-    return redirect('home')
-
 def register(request):
-    form_user = UserForm()
-    form_profile = UserProfileForm()
-
-    if request.method == 'POST':
-        form_user = UserForm(request.POST)
-        form_profile = UserProfileForm(request.POST, request.FILES)
-
-        if form_user.is_valid() and form_profile.is_valid():
-            user = form_user.save()
-            profile = form_profile.save(commit=False)
-            profile.user = user
-            profile.save()
-
-            login(request, user)
-            messages.success(request, 'Register Successfull!')
-
-            return redirect('home')
-
-    context = {
-        "form_user": form_user,
-        "form_profile": form_profile
-    }
-
-    return render(request, 'users/register.html',context)
-
-def user_login(request):
-    form = AuthenticationForm(request, data=request.POST)
-
+    form = RegistrationForm(request.POST or None) 
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already have an account!")
+        return redirect("blog:list")
     if form.is_valid():
-        # username = form.cleaned_data.get('username')
-        # password = form.cleaned_data.get('password')
-        # user = authenticate(username=username, password = password)
+        form.save()
+        name = form.cleaned_data["username"]
+        messages.success(request, f"Account created for {name}")
+        return redirect("login")  
+                
+    context = {
+        "form" : form
+    }
+    return render(request, "users/register.html", context)
 
-        user = form.get_user()
-
-        if user:
-            messages.success(request,'login successfull')
-            login(request, user)
-            return redirect('home')
-
-    return render(request, 'users/login.html', {"form":form})
+def profile(request):
+    u_form = UserUpdateForm(request.POST or None, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST or None, instance=request.user.profile, files=request.FILES)
+    
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        messages.success(request, "Your profile has been updated!")
+        return redirect(request.path)
+    
+    context = {
+        "u_form" : u_form,
+        "p_form" : p_form    
+    }
+    return render(request, "users/profile.html", context)
